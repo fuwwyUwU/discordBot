0f58a2a43181d16fdc79364904f070dc36396f57
fuwwyUwU <vsjoqvist@gmail.com>
2021-07-30 16:45:33 +0200

    tried to add prefixes

--- paste-bot/src/command.js
+++ paste-bot/src/command.js
@@ -1,17 +1,31 @@
-prefix = "?"
+const { mongoose } = require("mongoose")
+const mongo = require("./mongo")
+const prefix = require("./prefix")
+
+PREFIX = "?"
+
+
 
 module.exports = (client, aliases, callback) => {
+
+    const cache = {} //user.id [prefiix]
+
     if (typeof aliases === 'string'){
         aliases = [aliases]
     }
 
-    client.on('message', (message) => {
+    client.on('message', async (message) => {
         const { content } = message
+
+        await OnCommand(message.author)
+
+        cache[message.author.id] = [prefix]
+
         
         const lcontent = content.toLowerCase()
 
         aliases.forEach((alias) => {
-            const command = `${prefix}${alias}`.toLowerCase()   
+            const command = `${PREFIX}${alias}`.toLowerCase()   
 
             if (lcontent === command || lcontent.startsWith(`${command} `)){
                     console.log(`runing the command ${command}`)
@@ -19,4 +33,30 @@ module.exports = (client, aliases, callback) => {
             }
         })
     })
+}
+
+
+const OnCommand = async author => {
+
+
+    let data = cache[author.id]
+
+    if (!data){
+        console.log('FETCHIng FROM DATABASE')
+        await mongo().then(async mongoose => {
+            try {
+                const result = await PrefixSchema.findOne({_id: author.id})
+                cache[author.id] = data = [result.prefix]
+            }finally{
+                mongoose.connection.close()
+                PREFIX = [result.prefix]
+            }
+
+        })
+    }else {
+        PREFIX = cache[author.id].prefix
+    }
+
+
+
 }
