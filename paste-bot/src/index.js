b836a59f49f8f79fa2994c6ec5a95a6b4dc12f92
fuwwyUwU <vsjoqvist@gmail.com>
2021-08-07 16:39:10 +0200

    thung

--- paste-bot/src/index.js
+++ paste-bot/src/index.js
@@ -1,47 +1,29 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const {Client, Message } = require('discord.js');
+const { Client, Message } = require("discord.js");
 
-const mongo = require('./mongo.js');
+const mongo = require("./mongo.js");
 
-const PrefixSchema = require('./schemas/prefix-schema')
+const PrefixSchema = require("./schemas/prefix-schema");
 
-const client  = new Client({
-    partials: ['MESSAGE', 'REACTION']
+const client = new Client({
+  partials: ["MESSAGE", "REACTION"],
 });
-const prefix = require ('./prefix')
 
-const command = require('./command')
-
-
-
+const command = require("./command");
 
 client.login(process.env.DISCORD_BOT_TOKEN);
 
-client.on('ready', async () => {
+console.log(process.env.DISCORD_BOT_TOKEN);
 
+client.on("ready", async () => {
+  await mongo().then((mongoose) => {
+    try {
+      console.log("connncted to mongo");
+    } finally {
+      mongoose.connection.close();
+    }
+  });
 
-
-    await mongo().then(mongoose => {
-        try{
-
-            console.log('connncted to mongo')
-        }finally{
-        mongoose.connection.close()
-        }
-    })
-    
-    console.log(`${client.user.tag} is logged in`);
-
-    prefix(client)
-
-    command(client, ['test', 't'], message => {
-        return message.channel.send('tets')
-    })
-
-})
-
-
-
-
-
+  console.log(`${client.user.tag} is logged in`);
+});
